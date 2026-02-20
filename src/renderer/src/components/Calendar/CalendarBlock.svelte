a6bec07c2c1c0901bea97ef7394647faf4aed87d
Luhmeiy <medeiros.luizhenrique@yahoo.com.br>
2026-02-20 19:25:04 -0300

    feat(calendar): implement base layout

---
+++ src/renderer/src/components/Calendar/CalendarBlock.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	interface CalendarBlockProps {
+		isAchieved?: boolean
+		isDisabled?: boolean
+	}
+
+	let { isAchieved, isDisabled }: CalendarBlockProps = $props()
+</script>
+
+<div
+	class="w-full aspect-square border-2 border-darkblue rounded-xs"
+	class:border-zinc-500={isDisabled}
+	class:bg-darkblue={isAchieved}
+	class:bg-zinc-500={isDisabled && isAchieved}
+></div>
--- src/renderer/src/windows/Calendar.svelte
+++ src/renderer/src/windows/Calendar.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import { Conf } from 'electron-conf/renderer'
 	import type { Mode } from '$interfaces/Mode'
-	import ErrorModal from '$components/ErrorModal.svelte'
+	import CalendarBlock from '$components/Calendar/CalendarBlock.svelte'
 	import Header from '$components/Header.svelte'
 
 	const conf = new Conf()
+	const today = new Date()
 
-	let errorMessage = $state('')
+	const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+	const totalCells = 35
+
+	let month = $state(today.toLocaleString('en', { month: 'long' }))
+
+	let firstMonthDay = new Date(today.getFullYear(), today.getMonth(), 1).getDay()
+	let lastMonthDay = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate()
+	let trailingEmpty = totalCells - lastMonthDay - firstMonthDay
 
 	$effect(() => {
 		const getTheme = async (): Promise<void> => {
@@ -27,8 +35,26 @@
 	<Header closeAppToTray={false} headerTitle="Calendar" isCalendar />
 
 	<div
-		class="relative h-full bg-white flex flex-col items-center gap-4 px-2 py-6 border-3 border-darkblue rounded-b-lg overflow-y-auto dark:bg-zinc-800 darker:bg-zinc-950"
+		class="relative h-full bg-white flex flex-col items-center gap-1 px-9 py-6 border-3 border-darkblue rounded-b-lg overflow-y-auto dark:bg-zinc-800 darker:bg-zinc-950"
 	>
-		<ErrorModal bind:errorMessage />
+		<h2 class="font-bold">{month}</h2>
+
+		<div class="w-full grid grid-cols-7 gap-1">
+			{#each weekdays as weekday, i (i)}
+				<p class="text-center">{weekday}</p>
+			{/each}
+
+			{#each { length: firstMonthDay }}
+				<CalendarBlock isDisabled />
+			{/each}
+
+			{#each { length: lastMonthDay }}
+				<CalendarBlock />
+			{/each}
+
+			{#each { length: trailingEmpty }}
+				<CalendarBlock isDisabled />
+			{/each}
+		</div>
 	</div>
 </div>
